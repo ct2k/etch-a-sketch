cf5b4594a88ab10449a059ab11cd91d6d8fe71c2
ct2k <voicesfromthedarkside@protonmail.com>
2022-03-18 12:35:45 -0700

    hover trail effect working

--- js.js
+++ js.js
@@ -8,23 +8,39 @@ for (let i = 0; i < 256; i++) {
 
 }
 
-// const hoverEffect = containerBtn.querySelectorAll('div.grid')
-// console.log(hoverEffect)
+// Hover trailing effect
+// hoverEffect can = document or containerBtn, doesn't matter.
+// querySelectorAll can = .grid or .container > grid, doesn't matter.
+
+const hoverEffect = document.querySelectorAll('.grid')
+console.log(hoverEffect)
+
+for (let i = 0; i < hoverEffect.length; i++) {
+    hoverEffect[i].addEventListener('mouseover', () => {
+        hoverEffect[i].style.backgroundColor = 'black'
+    })
+}
+
+// const mouse = document.querySelector(hoverEffect)
+
+// mouse.addEventListener('click', () => {
+//     mouse.setAttribute('style', 'background-color: black')
+// })
 
 // Works only for the first '.grid' element or 'block'
 // Selecting '.grid' or '.container > .grid' makes no difference
 // Using just '.container' will make all the child element black
 // Tried multiple eventListeners. They only select the first block.
 
-const mouseOver = document.querySelector('.grid')
+// const mouseOver = document.querySelector('.grid')
 
-mouseOver.addEventListener('mouseenter', () => {
-    mouseOver.setAttribute('style', 'background-color: black')
-})
+// mouseOver.addEventListener('mouseenter', () => {
+//     mouseOver.setAttribute('style', 'background-color: black')
+// })
 
-mouseOver.addEventListener('mouseleave', () => {
-    mouseOver.removeAttribute('style')
-})
+// mouseOver.addEventListener('mouseleave', () => {
+//     mouseOver.removeAttribute('style')
+// })
 
 
 
