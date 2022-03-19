762cc631b2a803585ac87868064898031a9c608b
ct2k <voicesfromthedarkside@protonmail.com>
2022-03-18 19:10:13 -0700

    more attempts at making a 16x16 grid. no progress.

--- js.js
+++ js.js
@@ -2,18 +2,32 @@
 
 const containerBtn = document.querySelector('.container')
 
+// function createAGrid(num1, num2) {
+
+// for (let i = 0; i < num1; i++) {
+//     const createGrid = document.createElement('div')
+//     // createGrid.classList.add('grid')
+//     containerBtn.appendChild(createGrid)
+//     for (let j = 0; j < num2; j++) {
+//         const createBox = document.createElement('div')
+//         // createBox.classList.add('grid')
+//         containerBtn.appendChild(createBox)
+            
+//         }
+
+// }
+
+// }
+
 function createAGrid(num1, num2) {
 
 for (let i = 0; i < num1; i++) {
+    for (let j = 0; j < num2; j++) {
+
     const createGrid = document.createElement('div')
     // createGrid.classList.add('grid')
-    containerBtn.setAttribute('style', `width: ${num1}`)
     containerBtn.appendChild(createGrid)
-    for (let j = 0; j < num2; j++) {
-        const createBox = document.createElement('div')
-        // createBox.classList.add('grid')
-        containerBtn.setAttribute('style', `width: ${num2}`)
-        containerBtn.appendChild(createBox)
+   
             
         }
 
@@ -22,46 +36,47 @@ for (let i = 0; i < num1; i++) {
 }
 
 
-createAGrid(10, 10)
+createAGrid(16, 16)
 
 
 // Create the Hover trailing effect
 // hoverEffect can = document or containerBtn, doesn't matter.
 // querySelectorAll can = .grid or .container > grid, doesn't matter.
 
-// const hoverEffect = document.querySelectorAll('.grid')
+const hoverEffect = document.querySelectorAll('div')
 
-// console.log(hoverEffect)
+console.log(hoverEffect)
 
-// for (let i = 0; i < hoverEffect.length; i++) {
-//     hoverEffect[i].addEventListener('mouseover', () => {
-//         hoverEffect[i].setAttribute('style', 'background-color: black')
+for (let i = 0; i < hoverEffect.length; i++) {
+    hoverEffect[i].addEventListener('mouseover', () => {
+        // hoverEffect[i].classList.add('grid')
+        hoverEffect[i].setAttribute('style', 'background-color: orange')
 
-//     })
-// }
+    })
+}
 
 // Clear the grid
 
-// const clearBtn = document.querySelector('#clearbtn')
+const clearBtn = document.querySelector('#clearbtn')
 
-// clearBtn.addEventListener('click', () => {
-//     let userInput = prompt('enter new grid value');
-//     if (userInput === '100') {
-//         for (let i = 0; i < userInput; i++) {
-//             const createGrid = document.createElement('div')
-//             createGrid.classList.add('grid')
-//             containerBtn.appendChild(createGrid)
+clearBtn.addEventListener('click', () => {
+    // let userInput = prompt('enter new grid value');
+    // if (userInput === '100') {
+    //     for (let i = 0; i < userInput; i++) {
+    //         const createGrid = document.createElement('div')
+    //         createGrid.classList.add('grid')
+    //         containerBtn.appendChild(createGrid)
 
     
-//         }
-//     }
-//     for (let i = 0; i < hoverEffect.length; i++) {
-//         hoverEffect[i].removeAttribute('style')
+    //     }
+    // }
+    for (let i = 0; i < hoverEffect.length; i++) {
+        hoverEffect[i].removeAttribute('style')
         
-//     }
+    }
    
 
-// })
+})
 
 
 
